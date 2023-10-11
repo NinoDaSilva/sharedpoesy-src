0b34e36711f0c8963b662832c7610bc3b6fcb8c7
Nino DA SILVA <ninoportos@gmail.com>
2023-10-11 16:44:10 +0200

    update url

--- src/App.vue
+++ src/App.vue
@@ -97,7 +97,7 @@ Your poem here ...
 var connected = false;
 var pocketbase_ip = "";
 //if (import.meta.env.MODE === "production")
-pocketbase_ip = "https://pocketbase.hmabed.tech:443";
+pocketbase_ip = "https://sharedpoesy.nino-da-silva.fr:443";
 //else pocketbase_ip = "http://127.0.0.1:8090";
 const pb = new PocketBase(pocketbase_ip);
 var currentUser;
