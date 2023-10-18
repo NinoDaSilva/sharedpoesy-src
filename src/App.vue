a9ec1545da15a2f8fbc250031278eaae2ad636bd
Nino DA SILVA <ninoportos@gmail.com>
2023-10-18 16:21:47 +0200

    new poem

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
-  import SignIn from "./components/SignIn.vue";
-  import FamousPoets from "./components/FamousPoets.vue";
-  import PocketBase from "pocketbase";
+import SignIn from "./components/SignIn.vue";
+import PocketBase from "pocketbase";
 </script>
 
 <template>
@@ -16,45 +15,96 @@
     <div class="wrapper" id="signOut">
       <div><SignIn msg="Poet ! Tell us who you are !" /></div>
       <label>email: </label><br />
-      <input type="email" required id="email" placeholder="username@domain.tld"/>
-      <br/>
+      <input
+        type="email"
+        required
+        id="email"
+        placeholder="username@domain.tld"
+      /><br />
       <label>password: </label><br />
       <input type="password" required id="passwd" /><br />
       <button v-on:click="register()">Email SignUp</button>
       <button v-on:click="login()">Email SignIn</button>
       <button v-on:click="googlelogin()">Google SignIn</button>
-      <button v-on:click="githublogin()">GitHub SignIn</button>
+      <button v-on:click="githublogin()">Github SignIn</button>
       <p><label id="status"> You are not yet connected </label><br /></p>
     </div>
     <div class="hidden" id="addPoem">
       <div><SignIn msg="Write your poem !" /></div>
       <h3>The poem remains private, until you make it public</h3>
       <label>Poem's title</label><br />
-      <input type="text" required name="title" id="title" placeholder="edit me"/>
-      <br />
+      <input
+        type="text"
+        required
+        name="title"
+        id="title"
+        placeholder="edit me"
+      /><br />
       <label>Poem's content</label><br />
-      <textarea required name="content" id="content" placeholder="edit me" rows="10" cols="50">
+      <textarea
+        required
+        name="content"
+        id="content"
+        placeholder="edit me"
+        rows="10"
+        cols="50"
+      >
 Your poem here ...
       </textarea>
       <br />
+
+
+      <input
+        type="text"
+        required
+        name="langue"
+        id="langue"
+        placeholder="edit me"
+      /><br />
+      <label>Poem's language</label><br />
+      <textarea
+        required
+        name="text"
+        id="text"
+        placeholder="edit me"
+        rows="10"
+        cols="50"
+      >
+Your poem language here ...
+      </textarea>
+      <br />
+
+
+
       <label>Illustration: </label>
 
-      <input type="file" id="file" name="file" placeholder="my file" accept="image/png, image/jpeg"/>
-      <br />
+      <input
+        type="file"
+        id="file"
+        name="file"
+        placeholder="my file"
+        accept="image/png, image/jpeg"
+      /><br />
       <!--<img id="illustration" src="./assets/null.png" alt="poem illustration" width="75" height="75"/><br>-->
-      <input type="checkbox" id="notpublic" value="true" />
+      <input type="checkbox" id="notpublic"/>
       <label>Private poem</label>
       <br /><button v-on:click="createPoem()">Add the poem</button>
       <button v-on:click="fetchPoems()">List of poems</button><br />
-      <label for="poemtitle" id="poemtitle"
+      <label
+        for="poemtitle"
+        id="poemtitle"
         style="color: teal; font-weight: 500"
       >
         ...
       </label>
-      <img id="poemillustration" src="./assets/null.jpg" alt="poem illustration" width="75" height="75"
+      <img
+        id="poemillustration"
+        src="./assets/null.jpg"
+        alt="poem illustration"
+        width="75"
+        height="75"
         style="background-color: gray"
-      />
-      <br />
+      /><br />
       <textarea id="poemcontent" readonly rows="10" cols="50"> ... </textarea>
       <br />
       <button v-on:click="nextPoem()">Next poem</button><br />
@@ -67,112 +117,114 @@ Your poem here ...
 </template>
 
 <script>
-var connected = false;
-var pocketbase_ip = "";
-//if (import.meta.env.MODE === "production")
-pocketbase_ip = "https://sharedpoesy.nino-da-silva.fr:443";
-//else pocketbase_ip = "http://127.0.0.1:8090";
-const pb = new PocketBase(pocketbase_ip);
-var currentUser;
-export default {
-  methods: {
-    //this method allows a new user to sign up the system. Once done, the user receives an email
-    //asking for account validation. Once the validation made the user is added to the system
-    async login() {
-      await pb
-        .collection("users")
-        .authWithPassword(
-          document.getElementById("email").value,
-          document.getElementById("passwd").value
-        );
-
-      if (pb.authStore.isValid) {
-        document.getElementById("status").innerHTML = "You are now logged in";
-        connected = true;
-        currentUser = pb.authStore.model;
-        document.getElementById("signOut").style.visibility = "hidden";
-        document.getElementById("addPoem").style.visibility = "visible";
-      }
-    },
-    //this method allows the already registred user to log in the system.
-    async register() {
-      currentUser = await pb.collection("users").create({
-        email: document.getElementById("email").value,
-        password: document.getElementById("passwd").value,
-        passwordConfirm: document.getElementById("passwd").value,
-        name: "John Di",
-      });
-      if (currentUser) {
-        document.getElementById("status").innerHTML =
-          "Wainting for your email validation ...";
+  var poemsList;
+  var connected = false;
+  var pocketbase_ip = "";
+  //if (import.meta.env.MODE === "production")
+  pocketbase_ip = "https://sharedpoesy.kujundzic.fr:443";
+  //else pocketbase_ip = "http://127.0.0.1:8090";
+  const pb = new PocketBase(pocketbase_ip);
+  var currentUser;
+  export default {
+    methods: {
+      //this method allows a new user to sign up the system. Once done, the user receives an email
+      //asking for account validation. Once the validation made the user is added to the system
+      async login() {
         await pb
           .collection("users")
-          .requestVerification(document.getElementById("email").value);
-      }
-    },
-    async googlelogin() {
-      await pb.collection("users").authWithOAuth2({ provider: "google" });
-      if (pb.authStore.isValid) {
-        document.getElementById("status").innerHTML = "You are now logged in";
-        connected = true;
-        currentUser = pb.authStore.model;
-        document.getElementById("signOut").style.visibility = "hidden";
-        document.getElementById("addPoem").style.visibility = "visible";
-      }
-    },
-    async githublogin() {
-      await pb.collection("users").authWithOAuth2({ provider: "github" });
-      if (pb.authStore.isValid) {
-        document.getElementById("status").innerHTML = "You are now logged in";
-        connected = true;
-        currentUser = pb.authStore.model;
-        document.getElementById("signOut").style.visibility = "hidden";
-        document.getElementById("addPoem").style.visibility = "visible";
-      }
-    },
-    async createPoem() {
-      const record = await pb.collection("poems").create({
-        title: document.getElementById("title").value,
-        content: document.getElementById("content").value,
-        private: document.getElementById("notpublic").value,
-        email: currentUser.email,
-        illustration: document.getElementById("file").files[0],
-        langue: document.getElementById("langue").value,
-      });
-    },
-    async fetchPoems() {
-      //call a request filtering all accessible poems
-      var poemsList = await pb.collection('poems').getList(1, 50, { filter: 'email = "'+ currentUser.email+'" || private = false', });  //extract the number of readable poems
-      nbrPoems = poemsList.length;
-      //number of readable poems
-      var nbrPoems = poemsList.length;
-      //then display the first one
-      //if the number of readable poems is not null
-      if(nbrPoems > 0){
+          .authWithPassword(
+            document.getElementById("email").value,
+            document.getElementById("passwd").value
+          );
+
+        if (pb.authStore.isValid) {
+          document.getElementById("status").innerHTML = "You are now logged in";
+          connected = true;
+          currentUser = pb.authStore.model;
+          document.getElementById("signOut").style.visibility = "hidden";
+          document.getElementById("addPoem").style.visibility = "visible";
+        }
+      },
+      //this method allows the already registred user to log in the system.
+      async register() {
+        currentUser = await pb.collection("users").create({
+          email: document.getElementById("email").value,
+          password: document.getElementById("passwd").value,
+          passwordConfirm: document.getElementById("passwd").value,
+          name: "John Di",
+        });
+        if (currentUser) {
+          document.getElementById("status").innerHTML =
+            "Wainting for your email validation ...";
+          await pb
+            .collection("users")
+            .requestVerification(document.getElementById("email").value);
+        }
+      },
+      async googlelogin() {
+        await pb.collection("users").authWithOAuth2({ provider: "google" });
+        if (pb.authStore.isValid) {
+          document.getElementById("status").innerHTML = "You are now logged in with Google";
+          connected = true;
+          currentUser = pb.authStore.model;
+          document.getElementById("signOut").style.visibility = "hidden";
+          document.getElementById("addPoem").style.visibility = "visible";
+        }
+      },
+
+      async githublogin() {
+        await pb.collection("users").authWithOAuth2({ provider: "github" });
+        if (pb.authStore.isValid) {
+          document.getElementById("status").innerHTML = "You are now logged in with GitHub";
+          connected = true;
+          currentUser = pb.authStore.model;
+          document.getElementById("signOut").style.visibility = "hidden";
+          document.getElementById("addPoem").style.visibility = "visible";
+        }
+      },
+
+      async createPoem() {
+        var notpublic = false;
+        if (document.getElementById("notpublic").checked){
+          notpublic = true;
+        }
+        const record = await pb.collection("poems").create({
+          title: document.getElementById("title").value,
+          content: document.getElementById("content").value,
+          private: notpublic,
+          email: currentUser.email,
+          illustration: document.getElementById("file").files[0],
+          langue: document.getElementById("langue").value,
+        });
+      },
+      async fetchPoems() {
+        //call a request filtering all accessible poems
+        poemsList = await pb.collection('poems').getList(1, 50, { filter: 
+        'email = "’+ currentUser.email+’" || private = false', });
+        //extract the number of readable poems
+        nbpoems = poemsList.length;
+        //if the number of readable poems is not null
+        if(nbpoems>0){
+        //then display the first one
         document.getElementById('poemtitle').innerHTML=poemsList[0].title;
         document.getElementById('poemcontent').value=poemsList[0].content;
         document.getElementById('poemillustration').src=poemsList[0].illustration;
-      }
-      if(nbrPoems < - 1) {
-          nbrPoems ++;
-          document.getElementById('poemtitle').innerHTML=poemsList[nbrPoems].title;
-          document.getElementById('poemcontent').value=poemsList[nbrPoems].content;
-          document.getElementById('poemillustration').src=poemsList[nbrPoems].illustration;
         }
-      }
+        //store the indexof the currently displayed poem
+        currentPoem=0;
+      },
+      //this method allows the already registred user to log in the system.
     },
-    //this method allows the already registred user to log in the system.
-  },
-  nextPoem() {
-    //if the current displayed poem is not the last
-    //then
-    //skip to the next poem
-    //and display the new current poem
-    //document.getElementById('poemtitle').innerHTML=
-    //document.getElementById('poemcontent').value=
-    //document.getElementById('poemillustration').src=
-  },
-}
+    nextPoem() {
+      //if the current displayed poem is not the last  
+      //then
+      //skip to the next poem
+      //and display the new current poem
+      //document.getElementById('poemtitle').innerHTML=
+      //document.getElementById('poemcontent').value=
+      //document.getElementById('poemillustration').src=
+    },
+  };
 </script>
 
 <style>
